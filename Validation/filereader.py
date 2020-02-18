fbe9a91c2c5dab0549f8803de92064afe73fdc35
spockele <j.s.pockele@student.tudelft.nl>
2020-02-18 21:29:24 +0100

    Some work, might replace later :)

--- Validation/filereader.py
+++ Validation/filereader.py
@@ -40,19 +40,47 @@ def plot_aileron(node_dict: dict):
     Plot the node coordinates of the inp file
     :param node_dict: the return from read_inp()
     """
+    def find_shape_xz(x, y, z):
+        xco = []
+        zco = []
+        for i in range(len(y)):
+            if (x[i], y[i]) not in xco and (z[i], y[i]) not in zco:
+                xco.append((x[i], y[i]))
+                zco.append((z[i], y[i]))
+
+        return len(xco), len(zco)
+
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax1 = fig.add_subplot(111, projection='3d')
 
     xs = np.zeros((len(node_dict),))
     ys = np.zeros((len(node_dict),))
     zs = np.zeros((len(node_dict),))
+    ns = np.zeros((len(node_dict),))
+    dat = np.zeros((len(node_dict),))
 
     for node_num, node_coord in node_dict.items():
         xs[node_num - 1] = node_coord[0]
         ys[node_num - 1] = node_coord[1]
         zs[node_num - 1] = node_coord[2]
+        ns[node_num - 1] = node_num
+        dat[node_num - 1] = node_num
 
-    ax.scatter(xs, ys, zs)
+    ysort = np.argsort(ys)
+    xs, ys, zs, ns = xs[ysort], ys[ysort], zs[ysort], ns[ysort]
+    xsort = np.argsort(xs)
+    xs, ys, zs, ns = xs[xsort], ys[xsort], zs[xsort], ns[xsort]
+
+    print(list(xs))
+    print(list(zs))
+
+    print(find_shape_xz(xs, ys, zs))
+
+    img = ax1.scatter(xs, zs, ys, c=dat, cmap=plt.hsv())
+    ax1.set_xlabel("X")
+    ax1.set_ylabel("Z")
+    ax1.set_zlabel("Y")
+    fig.colorbar(img)
 
     plt.show()
 
