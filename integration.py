5f8c2e785a23ac85d35002a3bd996a1dd8a3b528
coarsedamavand <mrbfazaeli@gmail.com>
2020-02-19 09:41:57 +0100

    finished analytic integration method for one integrand

--- integration.py
+++ integration.py
@@ -8,22 +8,33 @@ import numpy as np
 
 # Analytic integration for a given polynomial
 class Polynomial:
-    def _init_(self, *coefficients):
+    
+    def __init__(self, *coefficients):
         self.coefficients = list(coefficients)
-        
-    def _call_(self,x):
+       
+    def __call__(self,x):
         layer = 0
         for coeff in self.coefficients:
             layer = coeff + x*layer
         return layer
     
-    def definite_integral(self):
+    def definite_integral(self, a, b):
         integr_coeffs= []
-        degree = len(self.coefficients)
-        exponent = len(self.coefficients) + 1
-        for i in range (len(self.coefficients) - 1):
-            integr_coeffs.append(self.coefficients[i]/exponent)
+        exponent = len(self.coefficients) - 1
+        for i in range (len(self.coefficients)):
+            integr_coeffs.append(self.coefficients[i]/(exponent+1))
             exponent -= 1
-        return Polynomial(*integr_coeffs)
-    
         
+        value = 0
+        degree = len(integr_coeffs)
+        for i in range(degree):
+            value += integr_coeffs[i]*(b^(degree-i) - a^(degree-i))
+        
+        return value
+    
+p = Polynomial(1, 0, 0, 0, 0)
+
+p_int = p.definite_integral(0, 1)
+
+print(p_int)
+
