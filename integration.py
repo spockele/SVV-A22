7dc726be79599f793dcf3b3bac4f042367087104
coarsedamavand <mrbfazaeli@gmail.com>
2020-02-19 10:31:12 +0100

    finalized for good, made more flexible

--- integration.py
+++ integration.py
@@ -11,6 +11,10 @@ class Polynomial:
     
     def __init__(self, *coefficients):
         self.coefficients = list(coefficients)
+        self.degree = len(self.coefficients) - 1
+        
+    def order(self):
+        self.degree = len(self.coefficients) - 1
        
     def __call__(self,x):
         layer = 0
@@ -18,23 +22,31 @@ class Polynomial:
             layer = coeff + x*layer
         return layer
     
-    def definite_integral(self, a, b):
-        integr_coeffs= []
-        exponent = len(self.coefficients) - 1
-        for i in range (len(self.coefficients)):
-            integr_coeffs.append(self.coefficients[i]/(exponent+1))
-            exponent -= 1
-        
+    def integrated(self, integrals):
+        for integral in range(integrals):
+            integr_coeffs= []
+            exponent = self.degree
+            for i in range (len(self.coefficients)):
+                integr_coeffs.append(self.coefficients[i]/(exponent+1))
+                exponent -= 1
+                
+            integr_coeffs.append(0.0)
+            self.coefficients = integr_coeffs
+            self.order()
+        print('Polynomial of Degree', self.degree)
+        print('Polynomial given by', self.coefficients)
+
+    
+    def definite_integral(self, a, b, integrals):
+        self.integrated(integrals)
         value = 0
-        degree = len(integr_coeffs)
-        for i in range(degree):
-            value += integr_coeffs[i]*(b^(degree-i) - a^(degree-i))
-        
+        for i in range(self.degree):
+            value += self.coefficients[i]*(b^(self.degree-i) - a^(self.degree-i))
+        print('Value of integral on (', a, ',', b, ') is', value)
         return value
     
-p = Polynomial(1, 0, 0, 0, 0)
+p = Polynomial(1)
 
-p_int = p.definite_integral(0, 1)
 
-print(p_int)
+p_int = p.definite_integral(0, 1, 3)
 
