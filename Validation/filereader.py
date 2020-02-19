0cfad0de978d5d3cbfe8c965c664a62ef67283f4
spockele <j.s.pockele@student.tudelft.nl>
2020-02-19 13:07:43 +0100

    Even more progress

--- Validation/filereader.py
+++ Validation/filereader.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 class Element():
-    def __init__(self, num, nodes, node_dct):
+    def __init__(self, num, nodes, node_dct, data_dict):
         self.num = num
         self.nodes = nodes
 
@@ -22,17 +22,37 @@ class Element():
         for i, corner in enumerate(self.corners):
             self.xs[i % 2, i // 2], self.ys[i % 2, i // 2], self.zs[i % 2, i // 2] = corner[0], corner[1], corner[2]
 
-    def __repr__(self):
-        return f'Element: nodes: {self.nodes}\ncorners: {self.corners}\n'
+        self.VMi_Bend = (data_dict["Bending"]["VM_S12"][self.num][0] + data_dict["Bending"]["VM_S12"][self.num][1]) / 2
+        self.S12_Bend = (data_dict["Bending"]["VM_S12"][self.num][2] + data_dict["Bending"]["VM_S12"][self.num][3]) / 2
 
-    def __str__(self):
-        return f'Element: nodes: {self.nodes}, corners: {self.corners}'
+        self.VMi_JBen = (data_dict["Jam_Bent"]["VM_S12"][self.num][0] + data_dict["Jam_Bent"]["VM_S12"][self.num][1]) / 2
+        self.S12_JBen = (data_dict["Jam_Bent"]["VM_S12"][self.num][2] + data_dict["Jam_Bent"]["VM_S12"][self.num][3]) / 2
+
+        self.VMi_JStr = (data_dict["Jam_Straight"]["VM_S12"][self.num][0] + data_dict["Jam_Straight"]["VM_S12"][self.num][1]) / 2
+        self.S12_JStr = (data_dict["Jam_Straight"]["VM_S12"][self.num][2] + data_dict["Jam_Straight"]["VM_S12"][self.num][3]) / 2
+
+    def __repr__(self):
+        return f'Element {self.num}: nodes: {self.nodes},'\
+               f' VMi: ({self.VMi_Bend}, {self.VMi_JBen}, {self.VMi_JStr}),'\
+               f' S12: ({self.S12_Bend}, {self.S12_JBen}, {self.S12_JStr})\n'
 
     def plot_self(self, ax):
         ax.plot_wireframe(self.xs, self.zs, self.ys)
 
 
-def read_inp():
+class Node():
+    def __init__(self, num, xyz, data_dict):
+        self.num = num
+        self.x, self.y, self.z = xyz
+        self.U_Bend = data_dict["Bending"]["displacement"][self.num]
+        self.U_JBen = data_dict["Jam_Bent"]["displacement"][self.num]
+        self.U_JStr = data_dict["Jam_Straight"]["displacement"][self.num]
+
+    def __repr__(self):
+        return f'Node {self.num}: ({self.x}, {self.y}, {self.z}), U: {self.U_Bend}, {self.U_JBen}, {self.U_JStr}\n'
+
+
+def read_inp(data_dict):
     """
     Reads the Abaqus input file
     :return:
@@ -43,6 +63,7 @@ def read_inp():
         elements = False
         s = False
         node_dct = {}
+        node_lst = []
         elem_lst = []
 
         for line in f:
@@ -66,7 +87,7 @@ def read_inp():
                 for i, data_pt in enumerate(data_line):
                     data_line[i] = int(data_pt)
 
-                elem_lst.append(Element(data_line[0], data_line[1:], node_dct))
+                elem_lst.append(Element(data_line[0], data_line[1:], node_dct, data_dict))
 
             if nodes and s:
                 data_line = line.strip("\n").replace(' ', '')
@@ -74,36 +95,50 @@ def read_inp():
                 for i, data_pt in enumerate(data_line):
                     data_line[i] = float(data_pt) if i > 0 else int(data_pt)
 
+                node_lst.append(Node(data_line[0], tuple(data_line[1:]), data_dict))
                 node_dct[data_line[0]] = tuple(data_line[1:])
 
             s = True
 
-    return node_dct, elem_lst
+    return node_lst, elem_lst
 
 
 def read_rpt():
     path = "B737_working_copy.rpt"
     with open(path) as f:
         case, tpe = False, False
+        data_dict = {
+                     "Bending":      {"VM_S12": {}, "displacement": {}, "reaction": {}},
+                     "Jam_Bent":     {"VM_S12": {}, "displacement": {}, "reaction": {}},
+                     "Jam_Straight": {"VM_S12": {}, "displacement": {}, "reaction": {}}
+                     }
         for line in f:
             if line == "\n":
                 case, tpe = False, False
 
             if case and tpe:
-                print(line)
+                data_line = line.strip("\n").split(",")
+                data_line = [int(data_line[0])] + [float(data) for data in data_line[1:]]
+                data_dict[case][tpe][data_line[0]] = tuple(data_line[1:])
+                print(case, ':', tpe, ':', data_line[0], ':', data_dict[case][tpe][data_line[0]])
 
             if "Step" in line:
-                case, tpe = line.strip("Step: ").split(", ")
+                case, tpe = line.strip("\n").replace("Step: ", "").split(", ")
 
-    return None, None, None
+    return data_dict
 
 
 if __name__ == '__main__':
-    case1, case2, case3 = read_rpt()
-    #node, elem = read_inp()
-    #fig = plt.figure()
-    #ax1 = fig.add_subplot(111, projection='3d')
-    #for el in elem:
-    #    el.plot_self(ax1)
+    data = read_rpt()
+    node, elem = read_inp(data)
+    print(node)
+    print(elem)
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111, projection='3d')
+    ax1.set_xlim(0, 1200)
+    ax1.set_ylim(0-600, 600)
+    ax1.set_zlim(0-600, 600)
+    for el in elem:
+        el.plot_self(ax1)
 
-    #plt.show()
+    plt.show()
