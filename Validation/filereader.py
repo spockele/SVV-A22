6bdadd81b46743b4fa53be8f6d3789de190076f4
spockele <j.s.pockele@student.tudelft.nl>
2020-02-20 15:28:30 +0100

    The plots work now!!!

--- Validation/filereader.py
+++ Validation/filereader.py
@@ -3,6 +3,8 @@ Reader for the .rpt and .inp files
 """
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as color
+import matplotlib.cm as cm
 import mpl_toolkits.mplot3d as axes3d
 import numpy as np
 
@@ -17,10 +19,7 @@ class Element():
             self.corners.append(node_dct[node])
 
         self.corners = self.corners[:-2] + [self.corners[-1]] + [self.corners[-2]]
-
-        self.xs, self.ys, self.zs = np.zeros((2, 2)), np.zeros((2, 2)), np.zeros((2, 2))
-        for i, corner in enumerate(self.corners):
-            self.xs[i % 2, i // 2], self.ys[i % 2, i // 2], self.zs[i % 2, i // 2] = corner[0], corner[1], corner[2]
+        self.xs, self.ys, self.zs = None, None, None
 
         self.VMi_Bend = (data_dict["Bending"]["VM_S12"][self.num][0] + data_dict["Bending"]["VM_S12"][self.num][1]) / 2
         self.S12_Bend = (data_dict["Bending"]["VM_S12"][self.num][2] + data_dict["Bending"]["VM_S12"][self.num][3]) / 2
@@ -37,14 +36,23 @@ class Element():
                f' S12: ({self.S12_Bend}, {self.S12_JBen}, {self.S12_JStr})\n'
 
     def plot_self(self, ax, case, tpe, VMi_mm, S12_mm):
-        if case == "Bend":
-            col = (self.VMi_Bend - VMi_mm[0]) / (VMi_mm[1] - VMi_mm[0]) if tpe == "VMi" else (self.S12_Bend - S12_mm[0]) / (S12_mm[1] - S12_mm[0])
-        elif case == "JBen":
-            col = (self.VMi_JBen - VMi_mm[0]) / (VMi_mm[1] - VMi_mm[0]) if tpe == "VMi" else (self.S12_JBen - S12_mm[0]) / (S12_mm[1] - S12_mm[0])
-        elif case == "JStr":
-            col = (self.VMi_JStr - VMi_mm[0]) / (VMi_mm[1] - VMi_mm[0]) if tpe == "VMi" else (self.S12_JStr - S12_mm[0]) / (S12_mm[1] - S12_mm[0])
+        self.xs, self.ys, self.zs = np.zeros((2, 2)), np.zeros((2, 2)), np.zeros((2, 2))
+        for i, corner in enumerate(self.corners):
+            corner.displace(case)
+            self.xs[i % 2, i // 2], self.ys[i % 2, i // 2], self.zs[i % 2, i // 2] = corner.xd, corner.yd, corner.zd
 
-        ax.plot_surface(self.xs, self.zs, self.ys)
+        if case == "Bending":
+            col = (self.VMi_Bend - VMi_mm[case][0]) / (VMi_mm[case][1] - VMi_mm[case][0]) if tpe == "VMi" else (self.S12_Bend - S12_mm[case][0]) / (S12_mm[case][1] - S12_mm[case][0])
+        elif case == "Jam_Bent":
+            col = (self.VMi_JBen - VMi_mm[case][0]) / (VMi_mm[case][1] - VMi_mm[case][0]) if tpe == "VMi" else (self.S12_JBen - S12_mm[case][0]) / (S12_mm[case][1] - S12_mm[case][0])
+        elif case == "Jam_Straight":
+            col = (self.VMi_JStr - VMi_mm[case][0]) / (VMi_mm[case][1] - VMi_mm[case][0]) if tpe == "VMi" else (self.S12_JStr - S12_mm[case][0]) / (S12_mm[case][1] - S12_mm[case][0])
+
+        #print(col)
+        cmap = plt.get_cmap('viridis')
+        col = [[cmap(col)]]
+
+        ax.plot_surface(self.xs, self.zs, self.ys, facecolors=col)
 
 
 class Node():
@@ -61,11 +69,11 @@ class Node():
         return f'Node {self.num}: ({self.x}, {self.y}, {self.z}), U: {self.U_Bend}, {self.U_JBen}, {self.U_JStr}\n'
 
     def displace(self, case):
-        if case == "Bend":
+        if case == "Bending":
             _, dx, dy, dz = self.U_Bend
-        elif case == "JBen":
+        elif case == "Jam_Bent":
             _, dx, dy, dz = self.U_JBen
-        elif case == "JStr":
+        elif case == "Jam_Straight":
             _, dx, dy, dz = self.U_JStr
         else:
             dx, dy, dz = 0, 0, 0
@@ -123,12 +131,11 @@ def read_inp(data_dict):
                 for i, data_pt in enumerate(data_line):
                     data_line[i] = float(data_pt) if i > 0 else int(data_pt)
 
-                node_lst.append(Node(data_line[0], tuple(data_line[1:]), data_dict))
-                node_dct[data_line[0]] = tuple(data_line[1:])
+                node_dct[data_line[0]] = Node(data_line[0], tuple(data_line[1:]), data_dict)
 
             s = True
 
-    return node_lst, elem_lst
+    return node_dct, elem_lst
 
 
 def read_rpt():
@@ -152,27 +159,38 @@ def read_rpt():
             if "Step" in line:
                 case, tpe = line.strip("\n").replace("Step: ", "").split(", ")
 
+        VMi_lst = {}
+        S12_lst = {}
         for case in data_dict:
-            for tpe in data_dict[case]:
-                VMi_lst = [(data_dict[case][tpe][node][0] + data_dict[case][tpe][node][1]) / 2 for node in data_dict[case][tpe]]
-                S12_lst = [(data_dict[case][tpe][node][2] + data_dict[case][tpe][node][3]) / 2 for node in data_dict[case][tpe]]
+            VMi_lst[case] = [(data_dict[case]["VM_S12"][node][0] + data_dict[case]["VM_S12"][node][1]) / 2 for node in data_dict[case]["VM_S12"]]
+            S12_lst[case] = [(data_dict[case]["VM_S12"][node][2] + data_dict[case]["VM_S12"][node][3]) / 2 for node in data_dict[case]["VM_S12"]]
 
-    return data_dict, (min(VMi_lst), max(VMi_lst)), (min(S12_lst), max(S12_lst))
+        VMi = {case: (min(VMi_lst[case]), max(VMi_lst[case])) for case in VMi_lst}
+        S12 = {case: (min(S12_lst[case]), max(S12_lst[case])) for case in S12_lst}
+
+    return data_dict, VMi, S12
 
 
 if __name__ == '__main__':
+    ipt = input("Load case and stress to display: ")
+    case, tpe = ipt.split(", ")
     data, VMi, S12 = read_rpt()
     node, elem = read_inp(data)
-    print(node)
-    print(elem)
+
+    mm = VMi[case] if tpe == "VMi" else S12[case]
+
     fig = plt.figure()
     ax1 = fig.add_subplot(111, projection='3d')
     ax1.set_ylim(0-600, 100)
     ax1.set_zlim(0-350, 350)
+
     for el in elem:
-        el.plot_self(ax1, "Bend", "VMi", VMi, S12)
+        el.plot_self(ax1, case, tpe, VMi, S12)
 
-    for no in node:
-        no.plot_self(ax1)
+    #for no in node:
+    #    no.plot_self(ax1)
 
+    n = color.Normalize(vmin=mm[0], vmax=mm[1])
+    m = cm.ScalarMappable(norm=n, cmap=plt.viridis())
+    plt.colorbar(m)
     plt.show()
