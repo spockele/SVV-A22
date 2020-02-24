998ab78370b65b1237a0e59c2fc163b7403d67fd
coarsedamavand <mrbfazaeli@gmail.com>
2020-02-24 16:55:10 +0100

    added givendata

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from matplotlib import cm
 import numpy as np
 from integration import Polynomial
 from interpolation import *
+from math import cos
 
 ############################# Data Setup ###################################
 #reading and obtaining the data points
@@ -61,6 +62,20 @@ zarray = np.append(zarray,-Ca)
 #Other Data
 ha = 0.173 #m
 z_hinge = ha/2 #m, z position of hinge line
+theta = 26*np.pi/180 #radians
+x1 = 0.149
+x2 = 0.554
+x3 = 1.541
+xa = 0.272
+xa1 = Ca/2 - xa/2
+xa2 = Ca/2 + xa/2
+y1 = 0.00681*cos(theta)
+y2 = 0
+y3 = 0.0203*cos(theta)
+z1 = 0
+z2 = 0
+z3 = 0
+za1 = 0
 
 ####################### Calculating the Distribution #########################
 # Unit test for correct center of pressure
@@ -71,13 +86,13 @@ z_hinge = ha/2 #m, z position of hinge line
 dq = np.array([])
 dqm = np.array([])
 cop = np.array([])
-#for i in range(Nx):
-#    splines = d3splines(datalist, zarray, i)
-#    dq_temp, dqm_temp, cop_temp = resultantload(splines, zarray, z_hinge)
-#    dq = np.append(dq, dq_temp)
-#    dqm = np.append(dqm, dqm_temp)
-#    cop = np.append(cop, cop_temp)
-## Find p[ol]
+for i in range(Nx):
+    splines = d3splines(datalist, zarray, i)
+    dq_temp, dqm_temp, cop_temp = resultantload(splines, zarray, z_hinge)
+    dq = np.append(dq, dq_temp)
+    dqm = np.append(dqm, dqm_temp)
+    cop = np.append(cop, cop_temp)
+# Find polynomial describibing the loading
 print(dq)
 print(dqm)
 print(cop)
@@ -86,5 +101,14 @@ print(cop)
 #lagrangePoly = lagrange(xarray,dq)
 #print(lagrangePoly)
 
+#As calculated, we have that the loading along the Span is as follows, corresponding to xarray:
 Loading = [-29509481109147.1, 973089896901375., -1.53534963623188e+16, 1.54143203855473e+17, -1.10423627336087e+18, 5.99623419445990e+18, -2.55711908765841e+19, 8.74175881223672e+19, -2.41726851774350e+20, 5.38505232159416e+20, -9.40780774833795e+20, 1.17417598503425e+21, -6.14999921565349e+20, -1.58790426834909e+21, 6.03895442263498e+21, -1.24603828547045e+22, 1.93975666854832e+22, -2.46837715938424e+22, 2.65182517394726e+22, -2.44386699684457e+22, 1.94870693141255e+22, -1.35070631821410e+22, 8.15551918173636e+21, -4.29130301166523e+21, 1.96583402334124e+21, -7.82313899450026e+20, 2.69564466254067e+20, -8.00707322662478e+19, 2.03880947538399e+19, -4.41931008930020e+18, 8.08578364084700e+17, -1.23594063750417e+17, 1.55855421427658e+16, -1.59663105896230e+15, 130364565874585., -8282687446888.95, 397088926359.289, -13796246243.3501, 328870936.981902, -4983645.55435716, 43270.9107925202, -211.492182015877, 0.0]
 print(Loading)
+
+
+
+
+
+# Reaction forces
+b = [z1, z2, z3, za1, y1, y2, y3, 0, 0, 0, 0, 0]
+print(b)
