f1544f2dae7cf72a064e39561bd596e0465b3874
coarsedamavand <mrbfazaeli@gmail.com>
2020-02-25 17:51:32 +0100

    finished with rxn forces

--- interpolation.py
+++ interpolation.py
@@ -3,6 +3,7 @@ from sympy import Symbol, simplify, Poly
 from functools import reduce
 import operator
 from integration import Polynomial
+import matplotlib.pyplot as plt
 
 def d3splines(data, zarray, x):
     # contructing the linalg equation Az=d to solve for z
@@ -53,19 +54,30 @@ def d3splines(data, zarray, x):
    
     return splines
 
-def lagrange(xvals, yvals):    
-    def _basis(j):
-        p = [(x - xvals[i])/(xvals[j] - xvals[i]) for i in range(n) if i != j]
-        return reduce(operator.mul, p) #multiplies all basis functinos together
+def intSpline(splines, xarray):
+    newPoints = np.array([0])
+    integral = 0
+    for x in range(len(xarray)-1):
+        if len(splines[0]) ==  4:
+            p = Polynomial(splines[x,0], splines[x,1], splines[x,2], splines[x,3])
+        else:
+            p = Polynomial(splines[x,0], splines[x,1])            
+        p = p.definite_integral(xarray[x], xarray[x+1], 1)
+        integral += p
+        newPoints = np.append(newPoints, integral)
+    newPoints = np.append(newPoints,0)
+    si = np.array([0, 0])
+    for z in range(len(xarray)-1):
+        fi0 = newPoints[z]
+        fi1 = newPoints[z+1]
+        zi0 = xarray[z]
+        zi1 = xarray[z+1]
+        spline = np.array([(fi1 - fi0)/(zi1 - zi0), fi0 - (fi1 - fi0)/(zi1 - zi0)*(zi0)]);
+        # splines are formatted as a_n, a_n-1, ... , a_0
+        si = np.vstack((si, spline))
+    newSplines = np.delete(si, 0, 0)
+    return newSplines, integral
     
-    x = Symbol('x')
-
-    n = len(xvals)
-    sigma = sum(_basis(j)*yvals[j] for j in range(n))
-    
-    LgnPoly = simplify(sigma)
-    LgnPoly = Poly(LgnPoly)       
-    return LgnPoly.all_coeffs()
 
 def resultantload(splines, zarray, z_hinge):
     #all the splines for the cross section are in this array now we integrate
@@ -88,3 +100,25 @@ def resultantload(splines, zarray, z_hinge):
     dqm = abs(dq*(z_hinge-cop))
     
     return dq, dqm, cop
+
+X = [ 0.00000000e+00,  1.24043255e-03,  6.19488340e-03,  1.60747104e-02,
+        3.08219348e-02,  5.03500141e-02,  7.45443496e-02,  1.03262959e-01,
+        1.36337311e-01,  1.73573312e-01,  2.14752446e-01,  2.59633057e-01,
+        3.07951768e-01,  3.59425026e-01,  4.13750765e-01,  4.70610180e-01,
+        5.29669598e-01,  5.90582435e-01,  6.52991229e-01,  7.16529741e-01,
+        7.80825101e-01,  8.45500000e-01,  9.10174899e-01,  9.74470259e-01,
+        1.03800877e+00,  1.10041757e+00,  1.16133040e+00,  1.22038982e+00,
+        1.27724924e+00,  1.33157497e+00,  1.38304823e+00,  1.43136694e+00,
+        1.47624755e+00,  1.51742669e+00,  1.55466269e+00,  1.58773704e+00,
+        1.61645565e+00,  1.64064999e+00,  1.66017807e+00,  1.67492529e+00,
+        1.68480512e+00,  1.68975957e+00, 1.69100000e+00]
+Y = [ 0.        , -0.20453439, -0.45610082, -0.73152864, -1.00626597,
+       -1.2745728 , -1.53301278, -1.7787306 , -2.00911488, -2.22178707,
+       -2.41469215, -2.58625995, -2.73543299, -2.86190878, -2.96620537,
+       -3.04955116, -3.11399564, -3.16205113, -3.19657674, -3.22017462,
+       -3.23515695, -3.24279353, -3.24331433, -3.23565556, -3.21761604,
+       -3.18604238, -3.13733611, -3.06766209, -2.97359769, -2.85247519,
+       -2.70267228, -2.5237452 , -2.31673031, -2.08407758, -1.8296829 ,
+       -1.55883877, -1.27815243, -0.99552932, -0.72027252, -0.46354811,
+       -0.24019092, -0.07830771,  0.        ]
+
